c2cf3502a93574ab71234ed225bd57a443ec1dc5
Owen Hsu <owen.hsu@rfdme.com>
2023-02-13 21:57:13 +0800

    update

--- pvplants-app/src/App.js
+++ pvplants-app/src/App.js
@@ -1,19 +1,59 @@
-import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import React, { useState } from 'react';
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
+import { Layout, Menu } from 'antd';
+import {
+  MenuUnfoldOutlined,
+  MenuFoldOutlined,
+  AppstoreOutlined,
+  FileOutlined,
+  SettingOutlined,
+} from '@ant-design/icons';
 import Dashboard from './Dashboard';
 import PlantDetails from './PlantDetails';
 import MaintenanceLogs from './MaintenanceLogs';
 
+const { Header, Sider, Content } = Layout;
+
 function App() {
+  const [collapsed, setCollapsed] = useState(false);
+
+  const toggleCollapsed = () => {
+    setCollapsed(!collapsed);
+  };
+
   return (
     <Router>
-      <div>
-        <Routes>
-          <Route exact path="/" element={<Dashboard />} />
-          <Route exact path="/plants/:id" element={<PlantDetails />} />
-          <Route exact path="/maintenance" element={<MaintenanceLogs />} />
-        </Routes>
-      </div>
+      <Layout style={{ minHeight: '100vh' }}>
+        <Sider trigger={null} collapsible collapsed={collapsed}>
+          <div className="logo" />
+          <Menu theme="dark" mode="inline" defaultSelectedKeys={['1']}>
+            <Menu.Item key="1" icon={<AppstoreOutlined />}>
+              <Link to="/">Dashboard</Link>
+            </Menu.Item>
+            <Menu.Item key="2" icon={<FileOutlined />}>
+              <Link to="/plants">Plants</Link>
+            </Menu.Item>
+            <Menu.Item key="3" icon={<SettingOutlined />}>
+              <Link to="/maintenance">Maintenance Logs</Link>
+            </Menu.Item>
+          </Menu>
+        </Sider>
+        <Layout className="site-layout">
+          <Header className="site-layout-background" style={{ padding: 0 }}>
+            {React.createElement(collapsed ? MenuUnfoldOutlined : MenuFoldOutlined, {
+              className: 'trigger',
+              onClick: toggleCollapsed,
+            })}
+          </Header>
+          <Content style={{ margin: '24px 16px', padding: 24, minHeight: 280 }}>
+            <Routes>
+              <Route exact path="/" element={<Dashboard />} />
+              <Route exact path="/plants" element={<PlantDetails />} />
+              <Route exact path="/maintenance" element={<MaintenanceLogs />} />
+            </Routes>
+          </Content>
+        </Layout>
+      </Layout>
     </Router>
   );
 }
