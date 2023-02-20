8172ebac3574f689bd0681dba7c55af9925dc713
sheauhei <sheauhei@gmail.com>
2023-02-20 17:16:10 +0800

    Update ems_sql_model.sql

--- sample/ems_sql_model.sql
+++ sample/ems_sql_model.sql
@@ -1,126 +1,201 @@
--- Microgrid table
-CREATE TABLE microgrids (
+CREATE TABLE SystemAdmin (
+    id SERIAL PRIMARY KEY,
+    username TEXT NOT NULL,
+    password TEXT NOT NULL,
+    email TEXT NOT NULL
+);
+
+CREATE TABLE Organization (
     id SERIAL PRIMARY KEY,
     name TEXT NOT NULL,
-    location TEXT NOT NULL,
-    capacity REAL NOT NULL,
-    status TEXT NOT NULL
+    address TEXT NOT NULL,
+    contact_person TEXT NOT NULL,
+    contact_email TEXT NOT NULL,
+    contact_phone TEXT NOT NULL,
+    admin_id INTEGER NOT NULL
 );
 
--- Device table
-CREATE TABLE devices (
+CREATE TABLE EMSUser (
     id SERIAL PRIMARY KEY,
-    name TEXT NOT NULL,
-    type TEXT NOT NULL,
-    manufacturer TEXT NOT NULL,
-    model TEXT NOT NULL,
-    location TEXT NOT NULL,
-    capacity REAL NOT NULL,
-    status TEXT NOT NULL,
-    microgrid_id INTEGER NOT NULL REFERENCES microgrids(id)
+    username TEXT NOT NULL,
+    password TEXT NOT NULL,
+    email TEXT NOT NULL,
+    role TEXT NOT NULL,
+    organization_id INTEGER NOT NULL REFERENCES Organization (id)
 );
 
--- SolarEnergy table (inherits from Device)
-CREATE TABLE solar_energy (
-    id INTEGER PRIMARY KEY REFERENCES devices(id),
-    generation_rate REAL NOT NULL
+-- Create Microgrid table
+CREATE TABLE Microgrid (
+  id SERIAL PRIMARY KEY,
+  name VARCHAR(255),
+  location VARCHAR(255),
+  max_load FLOAT,
+  max_generation FLOAT,
+  voltage FLOAT,
+  frequency FLOAT,
+  topology JSONB,
+  owner_id INTEGER REFERENCES Organization(id),
+  utility_id INTEGER REFERENCES Utility(id),
+  renewable_ppa_id INTEGER REFERENCES RenewablePPAContract(id),
+  system_settings_id INTEGER REFERENCES SystemSettings(id)
+);
+-- Create Device table
+CREATE TABLE Device (
+  id SERIAL PRIMARY KEY,
+  device_type  VARCHAR(255),
+  name VARCHAR(255),
+  description VARCHAR(255),
+  manufacturer VARCHAR(255),
+  model_number VARCHAR(255),
+  serial_number VARCHAR(255),
+  microgrid_id INTEGER REFERENCES Microgrid(id)
 );
 
--- WindEnergy table (inherits from Device)
-CREATE TABLE wind_energy (
-    id INTEGER PRIMARY KEY REFERENCES devices(id),
-    generation_rate REAL NOT NULL
+-- Create ESS table
+CREATE TABLE ESS (
+  id SERIAL PRIMARY KEY,
+  device_id INTEGER REFERENCES Device(id),
+  capacity FLOAT,
+  charge_efficiency FLOAT,
+  discharge_efficiency FLOAT,
+  max_charge_rate FLOAT,
+  max_discharge_rate FLOAT,
+  soc FLOAT
 );
 
--- ESS table (inherits from Device)
-CREATE TABLE ess (
-    id INTEGER PRIMARY KEY REFERENCES devices(id),
-    capacity REAL NOT NULL,
-    charge_rate REAL NOT NULL,
-    discharge_rate REAL NOT NULL,
-    soc REAL NOT NULL
+-- Create EVCharger table
+CREATE TABLE EVCharger (
+  id SERIAL PRIMARY KEY,
+  device_id INTEGER REFERENCES Device(id),
+  num_ports INTEGER,
+  max_charging_power FLOAT
 );
 
--- EVCharger table (inherits from Device)
-CREATE TABLE ev_chargers (
-    id INTEGER PRIMARY KEY REFERENCES devices(id),
-    charging_rate REAL NOT NULL,
-    num_ports INTEGER NOT NULL
+-- Create Load table
+CREATE TABLE Load (
+  id SERIAL PRIMARY KEY,
+  device_id INTEGER REFERENCES Device(id),
+  power FLOAT
 );
 
--- Load table (inherits from Device)
-CREATE TABLE loads (
-    id INTEGER PRIMARY KEY REFERENCES devices(id),
-    power REAL NOT NULL,
-    energy_consumption_today REAL NOT NULL,
-    energy_consumption_lifetime REAL NOT NULL
+-- Create SolarEnergy table
+CREATE TABLE SolarEnergy (
+  id SERIAL PRIMARY KEY,
+  device_id INTEGER REFERENCES Device(id),
+  capacity FLOAT,
+  efficiency FLOAT,
+  tilt_angle FLOAT,
+  azimuth_angle FLOAT
 );
 
--- LiveData table
-CREATE TABLE live_data (
+-- Create RenewableSourceFromPPA table
+CREATE TABLE RenewableSourceFromPPA (
+  id SERIAL PRIMARY KEY,
+  source_type VARCHAR(100), -- solar, wind, etc.
+  device_id INTEGER REFERENCES Device(id),
+  renewable_ppa_id INTEGER REFERENCES RenewablePPAContract(id)
+);
+
+-- Create LiveData table
+CREATE TABLE LiveData (
+    id SERIAL PRIMARY KEY,
+    timestamp TIMESTAMP,
+    device_id INTEGER REFERENCES Device(id),
+    data JSONB
+);
+
+CREATE TABLE ESSLiveData (
     id SERIAL PRIMARY KEY,
     timestamp TIMESTAMP NOT NULL,
-    device_id INTEGER NOT NULL REFERENCES devices(id),
-    data JSONB NOT NULL,
-    charge_rate REAL,
-    discharge_rate REAL,
-    soc REAL,
-    charging_rate REAL,
-    num_ports INTEGER,
-    power REAL,
-    energy_production_today REAL,
-    energy_production_this_week REAL,
-    energy_production_this_month REAL,
-    energy_production_this_year REAL,
-    energy_production_lifetime REAL,
-    energy_consumption_today REAL,
-    energy_consumption_this_week REAL,
-    energy_consumption_this_month REAL,
-    energy_consumption_this_year REAL,
-    energy_consumption_lifetime REAL,
-    generation_rate REAL
+    device_id INTEGER NOT NULL,
+    charge_rate FLOAT NOT NULL,
+    discharge_rate FLOAT NOT NULL,
+    soc FLOAT NOT NULL,
+    data JSONB NOT NULL
 );
 
--- PPA table
-CREATE TABLE ppas (
+CREATE TABLE EVChargerLiveData (
     id SERIAL PRIMARY KEY,
-    supplier TEXT NOT NULL,
-    price REAL NOT NULL,
-    capacity REAL NOT NULL,
-    term INTEGER NOT NULL,
-    start_date DATE NOT NULL,
-    end_date DATE NOT NULL,
-    contract_capacity REAL NOT NULL,
-    technology TEXT NOT NULL
+    timestamp TIMESTAMP NOT NULL,
+    device_id INTEGER NOT NULL,
+    charging_rate FLOAT NOT NULL,
+    num_ports INTEGER NOT NULL,
+    data JSONB NOT NULL
 );
 
--- RenewablePPAContract table (inherits from PPA)
-CREATE TABLE renewable_ppa_contracts (
-    id INTEGER PRIMARY KEY REFERENCES ppas(id),
-    from_solar_plant BOOLEAN NOT NULL,
-    from_wind_plant BOOLEAN NOT NULL,
-    solar_plant_name TEXT,
-    wind_plant_name TEXT
-);
-
--- Utility table
-CREATE TABLE utilities (
+CREATE TABLE LoadLiveData (
     id SERIAL PRIMARY KEY,
-    name TEXT NOT NULL,
-    location TEXT NOT NULL,
+    timestamp TIMESTAMP NOT NULL,
+    device_id INTEGER NOT NULL,
+    power FLOAT NOT NULL,
+    data JSONB NOT NULL
+);
+
+CREATE TABLE SolarEnergyLiveData (
+    id SERIAL PRIMARY KEY,
+    timestamp TIMESTAMP NOT NULL,
+    device_id INTEGER NOT NULL,
+    power FLOAT NOT NULL,
+    energy_production_today FLOAT NOT NULL,
+    energy_production_lifetime FLOAT NOT NULL,
+    energy_consumption_today FLOAT NOT NULL,
+    energy_consumption_lifetime FLOAT NOT NULL,
+    data JSONB NOT NULL
+);
+
+CREATE TABLE SolarPlantLiveData (
+    id SERIAL PRIMARY KEY,
+    timestamp TIMESTAMP NOT NULL,
+    plant_id INTEGER NOT NULL,
+    power FLOAT NOT NULL,
+    data JSONB NOT NULL
+);
+
+CREATE TABLE WindEnergyLiveData (
+    id SERIAL PRIMARY KEY,
+    timestamp TIMESTAMP NOT NULL,
+    device_id INTEGER NOT NULL,
+    power FLOAT NOT NULL,
+    data JSONB NOT NULL
+);
+
+CREATE TABLE WindPlantLiveData (
+    id SERIAL PRIMARY KEY,
+    timestamp TIMESTAMP NOT NULL,
+    plant_id INTEGER NOT NULL,
+    power FLOAT NOT NULL,
+    data JSONB NOT NULL
+);
+
+CREATE TABLE Utility (
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(255) NOT NULL,
+    location VARCHAR(255) NOT NULL,
     energy_sources JSONB NOT NULL,
-    peak_demand REAL NOT NULL,
+    peak_demand FLOAT NOT NULL,
     peak_demand_time TIME NOT NULL,
     energy_rates JSONB NOT NULL
 );
 
--- UtilityPriceContract table (inherits from Contract)
-CREATE TABLE utility_price_contracts (
-    id INTEGER PRIMARY KEY REFERENCES contracts(id),
-    utility_id INTEGER NOT NULL REFERENCES utilities(id),
-    energy_price REAL NOT NULL,
-    start_time TIME NOT NULL,
-    end_time TIME NOT NULL,
-    weekday_rates JSONB NOT NULL,
-    weekend_rates JSONB NOT NULL
+
+CREATE TABLE UtilityPriceContract (
+    id SERIAL PRIMARY KEY,
+    price FLOAT NOT NULL,
+    capacity FLOAT NOT NULL,
+    term INTEGER NOT NULL,
+    start_date DATE NOT NULL,
+    end_date DATE NOT NULL,
+    contract_capacity FLOAT NOT NULL,
+    microgrid_id INTEGER REFERENCES Microgrid(id),
+    utility_id INTEGER REFERENCES Utility(id)
 );
+CREATE TABLE RenewablePPAContract (
+    id SERIAL PRIMARY KEY,
+    supplier VARCHAR(255) NOT NULL,
+    price FLOAT NOT NULL,
+    capacity FLOAT NOT NULL,
+    term INTEGER NOT NULL,
+    start_date DATE NOT NULL,
+    end_date DATE NOT NULL,
+    contract_capacity FLOAT NOT NULL,
+    technology VARCHAR
